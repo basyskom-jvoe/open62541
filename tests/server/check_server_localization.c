1930b8cb63dade735964550350e24752f5c460a0
Jannis Voelker <jannis.voelker@basyskom.com>
2021-02-03 15:58:46 +0100

    Add tests for writing DisplayName and Description via callback

--- tests/server/check_server_localization.c
+++ tests/server/check_server_localization.c
@@ -16,6 +16,9 @@ UA_Server *server;
 UA_Boolean running;
 THREAD_HANDLE server_thread;
 
+UA_LocalizedText displayNameDe;
+UA_LocalizedText descriptionDe;
+
 static UA_StatusCode readDisplayName(UA_Server *s, const UA_NodeId *sessionId,
                                      void *sessionContext, const UA_NodeId *nodeId,
                                      void *nodeContext, size_t localeIdsSize,
@@ -29,7 +32,7 @@ static UA_StatusCode readDisplayName(UA_Server *s, const UA_NodeId *sessionId,
     const UA_String de = UA_STRING("de");
 
     if (localeIdsSize && localeIds && UA_String_equal(localeIds, &de))
-        *value = UA_LOCALIZEDTEXT_ALLOC("de", "MeinDisplayName");
+        UA_LocalizedText_copy(&displayNameDe, value);
     else
         *value = UA_LOCALIZEDTEXT_ALLOC("en", "MyDisplayName");
 
@@ -49,11 +52,52 @@ static UA_StatusCode readDescription(UA_Server *s, const UA_NodeId *sessionId,
     const UA_String de = UA_STRING("de");
 
     if (localeIdsSize && localeIds && UA_String_equal(localeIds, &de))
-        *value = UA_LOCALIZEDTEXT_ALLOC("de", "MeineBeschreibung");
+        UA_LocalizedText_copy(&descriptionDe, value);
     else
         *value = UA_LOCALIZEDTEXT_ALLOC("en", "MyDescription");
 
     return UA_STATUSCODE_GOOD;
+}
+
+static UA_StatusCode writeDisplayName(UA_Server *s, const UA_NodeId *sessionId,
+                                     void *sessionContext, const UA_NodeId *nodeId,
+                                     void *nodeContext, const UA_LocalizedText *value) {
+    (void)s;
+    (void)sessionId;
+    (void)sessionContext;
+    (void)nodeId;
+    (void)nodeContext;
+
+    const UA_String de = UA_STRING("de");
+
+    if (UA_String_equal(&value->locale, &de)) {
+        UA_LocalizedText_clear(&displayNameDe);
+        UA_LocalizedText_copy(value, &displayNameDe);
+        return UA_STATUSCODE_GOOD;
+    }
+
+
+    return UA_STATUSCODE_BADWRITENOTSUPPORTED;
+}
+
+static UA_StatusCode writeDescription(UA_Server *s, const UA_NodeId *sessionId,
+                                     void *sessionContext, const UA_NodeId *nodeId,
+                                     void *nodeContext, const UA_LocalizedText *value) {
+    (void)s;
+    (void)sessionId;
+    (void)sessionContext;
+    (void)nodeId;
+    (void)nodeContext;
+
+    const UA_String de = UA_STRING("de");
+
+    if (UA_String_equal(&value->locale, &de)) {
+        UA_LocalizedText_clear(&descriptionDe);
+        UA_LocalizedText_copy(value, &descriptionDe);
+        return UA_STATUSCODE_GOOD;
+    }
+
+    return UA_STATUSCODE_BADWRITENOTSUPPORTED;
 }
 
 static void
@@ -61,6 +105,7 @@ addTestVariable(void) {
     UA_DateTime now = 0;
     UA_VariableAttributes attr = UA_VariableAttributes_default;
     attr.accessLevel = UA_ACCESSLEVELMASK_READ | UA_ACCESSLEVELMASK_WRITE;
+    attr.writeMask = UA_WRITEMASK_DISPLAYNAME | UA_WRITEMASK_DESCRIPTION;
     UA_Variant_setScalar(&attr.value, &now, &UA_TYPES[UA_TYPES_DATETIME]);
 
     UA_NodeId currentNodeId = UA_NODEID_STRING(1, "test-variable");
@@ -79,7 +124,9 @@ addLocalizedAttributeSourceToTestVariable(void) {
     UA_NodeId currentNodeId = UA_NODEID_STRING(1, "test-variable");
     UA_LocalizedAttributeSource source;
     source.readDisplayName = readDisplayName;
+    source.writeDisplayName = writeDisplayName;
     source.readDescription = readDescription;
+    source.writeDescription = writeDescription;
     UA_StatusCode retval = UA_Server_setNodeLocalizedAttributeSource(server, currentNodeId, source);
     ck_assert_uint_eq(retval, UA_STATUSCODE_GOOD);
 }
@@ -92,6 +139,9 @@ THREAD_CALLBACK(serverloop) {
     return 0;
 }
 static void setup(void) {
+    displayNameDe = UA_LOCALIZEDTEXT_ALLOC("de", "MeinDisplayName");
+    descriptionDe = UA_LOCALIZEDTEXT_ALLOC("de", "MeineBeschreibung");
+
     running = true;
     server = UA_Server_new();
     UA_ServerConfig_setDefault(UA_Server_getConfig(server));
@@ -106,6 +156,9 @@ static void teardown(void) {
     THREAD_JOIN(server_thread);
     UA_Server_run_shutdown(server);
     UA_Server_delete(server);
+
+    UA_LocalizedText_clear(&displayNameDe);
+    UA_LocalizedText_clear(&descriptionDe);
 }
 
 START_TEST(client_readDisplayNameAttribute) {
@@ -224,6 +277,80 @@ START_TEST(client_readDescriptionAttributeNoLocale) {
     }
 END_TEST
 
+START_TEST(client_writeDisplayNameAttribute) {
+        UA_Client *client = UA_Client_new();
+        UA_ClientConfig *conf = UA_Client_getConfig(client);
+        UA_ClientConfig_setDefault(conf);
+
+        conf->sessionLocaleIdsSize = 1;
+        conf->sessionLocaleIds = UA_LocaleId_new();
+        *conf->sessionLocaleIds = UA_STRING_ALLOC("de");
+
+        UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://localhost:4840");
+        ck_assert_uint_eq(retval, UA_STATUSCODE_GOOD);
+
+        const UA_NodeId nodeId = UA_NODEID_STRING(1, "test-variable");
+
+        UA_LocalizedText updateValue = UA_LOCALIZEDTEXT("de", "MeinDisplayName2");
+
+        retval = UA_Client_writeDisplayNameAttribute(client, nodeId, &updateValue);
+        ck_assert_uint_eq(retval, UA_STATUSCODE_GOOD);
+
+        UA_LocalizedText result;
+        UA_LocalizedText_init(&result);
+
+        retval = UA_Client_readDisplayNameAttribute(client, nodeId, &result);
+        ck_assert_uint_eq(retval, UA_STATUSCODE_GOOD);
+
+        const UA_String expectedLocale = UA_STRING("de");
+        const UA_String expectedText = UA_STRING("MeinDisplayName2");
+
+        ck_assert(UA_String_equal(&expectedLocale, &result.locale));
+        ck_assert(UA_String_equal(&expectedText, &result.text));
+
+        UA_LocalizedText_clear(&result);
+        UA_Client_disconnect(client);
+        UA_Client_delete(client);
+    }
+END_TEST
+
+START_TEST(client_writeDescriptionAttribute) {
+        UA_Client *client = UA_Client_new();
+        UA_ClientConfig *conf = UA_Client_getConfig(client);
+        UA_ClientConfig_setDefault(conf);
+
+        conf->sessionLocaleIdsSize = 1;
+        conf->sessionLocaleIds = UA_LocaleId_new();
+        *conf->sessionLocaleIds = UA_STRING_ALLOC("de");
+
+        UA_StatusCode retval = UA_Client_connect(client, "opc.tcp://localhost:4840");
+        ck_assert_uint_eq(retval, UA_STATUSCODE_GOOD);
+
+        const UA_NodeId nodeId = UA_NODEID_STRING(1, "test-variable");
+
+        UA_LocalizedText updateValue = UA_LOCALIZEDTEXT("de", "MeineBeschreibung2");
+
+        retval = UA_Client_writeDisplayNameAttribute(client, nodeId, &updateValue);
+        ck_assert_uint_eq(retval, UA_STATUSCODE_GOOD);
+
+        UA_LocalizedText result;
+        UA_LocalizedText_init(&result);
+
+        retval = UA_Client_readDisplayNameAttribute(client, nodeId, &result);
+        ck_assert_uint_eq(retval, UA_STATUSCODE_GOOD);
+
+        const UA_String expectedLocale = UA_STRING("de");
+        const UA_String expectedText = UA_STRING("MeineBeschreibung2");
+
+        ck_assert(UA_String_equal(&expectedLocale, &result.locale));
+        ck_assert(UA_String_equal(&expectedText, &result.text));
+
+        UA_LocalizedText_clear(&result);
+        UA_Client_disconnect(client);
+        UA_Client_delete(client);
+    }
+END_TEST
+
 static Suite* testSuite_localization(void) {
     Suite *s = suite_create("Server localization");
     TCase *localizationCallback = tcase_create("server localization");
@@ -233,6 +360,8 @@ static Suite* testSuite_localization(void) {
     tcase_add_test(localizationCallback, client_readDisplayNameAttributeNoLocale);
     tcase_add_test(localizationCallback, client_readDescriptionAttribute);
     tcase_add_test(localizationCallback, client_readDescriptionAttributeNoLocale);
+    tcase_add_test(localizationCallback, client_writeDisplayNameAttribute);
+    tcase_add_test(localizationCallback, client_writeDescriptionAttribute);
 
     suite_add_tcase(s, localizationCallback);
     return s;
